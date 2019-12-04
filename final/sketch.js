f8c27e15ba65b3118722db99ec50ea0a2dce35c0
Vinnie <vz377@nyu.edu>
2019-12-04 17:13:03 -0500

    reorganizing file structure

--- final/sketch.js
+++ final/sketch.js
@@ -5,24 +5,60 @@ var world;
 var markerHiro, markerZb;
 
 function setup() {
-  // create our world (this also creates a p5 canvas for us)
-  world = new World('ARScene');
+    world = new World('ARScene');
+    var xOffset = 0;
+    var yOffset = 0;
 
-  // grab a reference to our two markers that we set up on the HTML side (connect to it using its 'id')
-  markerHiro = world.getMarker('hiro');
-  markerZb = world.getMarker('zb');
-    allMarkers = [markerHiro, markerZb];
+    marker1 = world.getMarker('hiro');
+    marker2 = world.getMarker('zb');
+    marker3 = world.getMarker('kanji');
+    marker4 = world.getMarker('nyu');
+    allMarkers = [marker1, marker2, marker3, marker4];
     for (var i = 0; i < allMarkers.length; i++){
-        tempPlane = new Fplane();
-        allMarkers[i].addChild(tempPlane);
+        xOffset = 0
+        zOffset = 0
+        for (var j = 0; j < 4; j++){
+            allMarkers[i].addChild(new Fbox(xOffset, zOffset));
+            xOffset++
+            if (xOffset >= 2){
+                xOffset = 0;
+                zOffset++
+            }
+        }
+        
+        //Hide Tiles
+        if (i == 0){
+            allMarkers[i].children[0]
+            allMarkers[i].children[1].hide()
+            allMarkers[i].children[2].hide()
+            allMarkers[i].children[3].hide()
+        }
+        if (i == 1){
+            allMarkers[i].children[0].hide()
+            allMarkers[i].children[1]
+            allMarkers[i].children[2].hide()
+            allMarkers[i].children[3].hide()
+        }
+        if (i == 2){
+            allMarkers[i].children[0].hide()
+            allMarkers[i].children[1].hide()
+            allMarkers[i].children[2]
+            allMarkers[i].children[3].hide()
+        }
+        if (i == 3){
+            allMarkers[i].children[0].hide()
+            allMarkers[i].children[1].hide()
+            allMarkers[i].children[2].hide()
+            allMarkers[i].children[3]
+        }
     }
-    added = false;
 }
 
 
 function draw() {
   // erase the background
   world.clearDrawingCanvas();
+    
     for (var i = 0; i < allMarkers.length; i++){
         if (allMarkers[i].isVisible() == true){
             for (var j = 0; j < allMarkers.length; j++){
@@ -32,43 +68,47 @@ function draw() {
                     textSize(50);
                     textAlign(CENTER);
                     text('2 things are visible', width/2, height/2);
-
-                   if (added == false){
-                       allMarkers[i].addChild(allMarkers[j].children[0])
-                       allMarkers[j].addChild(allMarkers[i].children[0])
-                       
-                       repositionPlanes(allMarkers);
-                       
-                       added = true;
-                   }
+                    
+                    if ((allMarkers[0].isVisible() && allMarkers[1].isVisible()) || (allMarkers[1].isVisible() && allMarkers[0].isVisible())){
+                        allMarkers[i].children[j].show();
+                    }
+                    
+                    if ((allMarkers[0].isVisible() && allMarkers[2].isVisible()) || (allMarkers[2].isVisible() && allMarkers[0].isVisible())){
+                        allMarkers[i].children[j].show();
+                    }
+                    
+                    if ((allMarkers[1].isVisible() && allMarkers[2].isVisible()) || (allMarkers[2].isVisible() && allMarkers[1].isVisible())){
+                        allMarkers[i].children[j].show();
+                    }
+                    
+                    if ((allMarkers[0].isVisible() && allMarkers[3].isVisible()) || (allMarkers[3].isVisible() && allMarkers[0].isVisible())){
+                        allMarkers[i].children[j].show();
+                    }
+                    
+                    if ((allMarkers[1].isVisible() && allMarkers[3].isVisible()) || (allMarkers[3].isVisible() && allMarkers[1].isVisible())){
+                        allMarkers[i].children[j].show();
+                    }
+                    
+                    if ((allMarkers[2].isVisible() && allMarkers[3].isVisible()) || (allMarkers[3].isVisible() && allMarkers[2].isVisible())){
+                        allMarkers[i].children[j].show();
+                    }
                 }
             }
         }
     }
 }
 
-class Fplane{
+class Fbox{
     
-    constructor(){
-        this.fPlane = new Plane({
-            x:0, y:0, z:0,
-            height: 1, width: 1,
+    constructor(xOffset, zOffset){
+        this.xOffset = xOffset
+        this.zOffset = zOffset
+        this.box1 = new Box({
+            x: this.xOffset - 0.5, y:0, z: this.zOffset -0.5,
+            height: 1, width: 1, depth: 0.5,
             rotationX:-90,
             red: random(255), green: random(255), blue: random(255)
         })
-        return this.fPlane
-    }
-}
-
-function repositionPlanes(x){
-    var xOffset
-    for (var i = 0; i < x.length; i++){
-        console.log(x[i])
-        xOffset = 0
-        for (var j = 0; j < x[i].children.length; j++){
-            console.log(x[i].children[j]);
-            x[i].children[j].x += xOffset
-            xOffset += 1
-        }
+        return this.box1
     }
 }
